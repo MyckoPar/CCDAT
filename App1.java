bb51937cd2428cbcc9c0ce3a110ab8d7f6797d28
MyckoPar <111344149+MyckoPar@users.noreply.github.com>
2022-08-16 16:09:46 +0800

    ADDED FILE

---
+++ App1.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+       // String[] favetourdestination= {"Boracay", "chocolatehills", "calle crisologo", "El nido", "Puerto prinsesa"};
+       
+       //1st
+
+        // most favorite
+        // System.out.println(favetourdestination[0]);
+        // least favorite
+        //System.out.println(favetourdestination[4]);
+ 
+
+
+
+       
+        //for (int x = 0; x < 5; x++){
+        //    System.out.println(favetourdestination[x]);
+        //}
+
+      //2nd
+
+     // Stack <String> qqq = new Stack<>();
+      
+      //qqq.push("book1");
+      //qqq.push("book2");
+      //qqq.push("book3");
+      //qqq.push("book4");
+      //qqq.push("book5");
+      
+
+      //qqq.pop();
+      //System.out.println(qqq);
+
+
+
+     //3
+     Queue<String> restoReservation = new LinkedList<>();
+     restoReservation.add("MIKE");
+     restoReservation.add("VINCENT");
+     restoReservation.add("LUIS");
+
+
+     System.out.println("The queue is: " + restoReservation );
+     restoReservation.remove();
+
+     System.out.println("The queue is: " + restoReservation );
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
