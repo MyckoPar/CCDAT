f01722ed9388c19087b6955f31452581d9d3e114
MyckoPar <111344149+MyckoPar@users.noreply.github.com>
2022-08-26 22:06:23 +0800

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,10 +1,18 @@
 public class Weapon {
-    // Weapon attributes
-    String object;
-    String name;
-    int damage;
-    float Weight;
-    String Rarity;
-    String Description;
+  // Weapon attributes
+  String object;
+  String name;
+  int damage;
+  double Weight;
+  String Rarity;
+  String Description;
+
+  // Weapon methods
+  public void Weaponlore() {
+    System.out.println("Weapon: " + name);
+    System.out.println("Damage:" + damage);
+    System.out.println("Weight: " + Weight);
+    System.out.println("Rarity: " + Rarity);
+    System.out.println("Description: " + Description);
   }
 }
